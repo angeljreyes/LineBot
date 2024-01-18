e87928ff4e34a145308300c779f7b2765064e690
DestopLine <50529879+DestopLine@users.noreply.github.com>
2024-01-18 15:57:11 -0400

    Refactor listeners

--- src/extensions/listeners.py
+++ src/extensions/listeners.py
@@ -16,96 +16,114 @@ class Listeners(commands.Cog):
 		@self.bot.tree.error
 		async def error_handler(interaction: discord.Interaction, error: Exception) -> None:
 			error_msg = None
+			note = str(error)
 
-			# An exception object and its error message
-			error_messages = {
-				discord.HTTPException: {
-					'Must be 2000 or fewer in length': 'El mensaje supera los 2000 caracteres'
-				},
-				commands.CommandNotFound: '',
-				commands.BadArgument: {
-					'Member': 'No se encontró a ese usuario en este servidor',
-					'User': 'No se encontró a ese usuario',
-					'Role': 'No se encontró ese rol',
-					'Channel': 'No se encontró ese canal'
-				},
-				commands.ArgumentParsingError: 'Hiciste un mal uso de las comillas',
-				commands.NoPrivateMessage: 'Este comando solo se puede usar en servidores',
-				commands.MaxConcurrencyReached: lambda: f'Se alcanzó el límite de usuarios para este comando: `{error.number}{"" if error.per == commands.BucketType.default else f" por {core.bucket_types[error.per]}"}`',
-				commands.MissingPermissions: lambda: f'No tienes los permisos requeridos para ejecutar este comando: ' + ', '.join((f'`{perm.replace("_", " ")}`' for perm in error.missing_perms)),
-				commands.NSFWChannelRequired: 'Este comando solo se puede usar en canales NSFW',
-				app_commands.CommandOnCooldown: lambda: f'Tienes que esperar **{core.fix_delta(timedelta(seconds=error.retry_after), ms=True)}** para ejecutar este comando de nuevo',
-				# Sometimes CommandInvokeError is raised, so the error is
-				# identified by the first characters of the error string
-				app_commands.CommandInvokeError: {
-					'403 Forbidden (error code: 50013): Missing Permissions': 'No tengo los permisos requeridos para ejecutar este comando. Intenta añadirlos editando los permisos o la posición mi rol',
-					'Unknown Message': 'El mensaje que se trató de editar/eliminar/reaccionar fue eliminado',
-					'.value: Must be 1024 or fewer in length.': 'El valor de una sección de un embed no debe ser mayor a 1024 caracteres',
-					'Invalid Form Body\nIn data.content: Must be 2000 or fewer in length': 'El mensaje no debe superar los 2000 caracteres',
-					'Invalid Form Body\nIn data.content: Must be 4000 or fewer in length': 'El mensaje no debe superar los 4000 caracteres',
-					'ValueError: Tag name limit reached': 'El límite de caracteres para el nombre un tag es de 32',
-					'ValueError: Invalid characters detected': 'El nombre de un tag no puede contener espacios ni caracteres markdown',
-					'ValueError: Invalid URL': 'URL inválida'
-				},
-				# Custom exceptions
-				exceptions.DisabledTagsError: '',
-				exceptions.ExistentTagError: 'Este tag ya existe', #---
-				exceptions.ImageNotFound: 'No se encontró ninguna imagen en los últimos 30 mensajes',
-				exceptions.NotOwner: 'Comando exclusivo del dueño del bot',
-				exceptions.BlacklistUserError: 'Estás en la lista negra. No tienes permitido usar el bot',
-				exceptions.NonExistentTagError: {
-					'This tag': 'Este tag no existe', #---
-					'This user': 'Este usuario no tiene tags', #---
-					'This server': 'Este servidor no tiene tags' #---
-				}
-			}
+			match error:
 
-			# If the error message hasn't been determined yet
-			if error_msg is None:
-				# Check if the error type is in the error_messages list
-				for exception_type in error_messages:
-					if isinstance(error, exception_type):
-						# Check if the error message is a dict (is variable)
-						if isinstance(error_messages[exception_type], dict):
-							for string in error_messages[exception_type]:
-								if string in str(error):
-									error_msg = error_messages[exception_type][string]
-									break
-						else:
-							error_msg = error_messages[exception_type]
-							break
+				case discord.HTTPException():
+					if 'Must be 2000 or fewer in length' in note:
+						error_msg = 'El mensaje supera los 2000 caracteres'
+				
+				case commands.CommandNotFound():
+					error_msg = None
 
-			# If error_msg is a lambda function, run it
-			if not isinstance(error_msg, str) and error_msg is not None:
-				error_msg = error_msg()
+				case commands.BadArgument():
+					if 'Member' in note:
+						error_msg = 'No se encontró a ese usuario en este servidor'
+					elif 'User' in note:
+						error_msg = 'No se encontró a ese usuario'
+					elif 'Role' in note:
+						error_msg = 'No se encontró ese rol'
+					elif 'Channel' in note:
+						error_msg = 'No se encontró ese canal'
 
-			# If the error remains unidentified, send a generic error message
-			if error_msg is None:
-				embed = discord.Embed(
-					title=f'Ha ocurrido un error',
-					description=f'```py\n{repr(error)}\n```',
-					colour=db.default_color(interaction)
-				).set_author(
-					name=interaction.user.name,
-					icon_url=interaction.user.display_avatar.url
-				).set_footer(text='Este error ha sido notificado y será investigado para su posterior correción.')
-				if not interaction.response.is_done():
-					await interaction.response.send_message(embed=embed, ephemeral=True)
-				else:
-					await interaction.edit_original_response(content=None, embed=embed, attachments=[], view=None)
-					await (await interaction.original_response()).clear_reactions()
-				core.logger.error('An error has ocurred', exc_info=error)
-				return
+				case commands.NoPrivateMessage():
+					error_msg = 'Este comando solo se puede usar en servidores'
+
+				case commands.MaxConcurrencyReached(number=number, per=per):
+					error_msg = f'Se alcanzó el límite de usuarios para este comando: {number}'
+					if per != commands.BucketType.default:
+						error_msg += f' por {core.bucket_types[per]}'
+
+				case commands.MissingPermissions(missing_permissions=perms):
+					error_msg = (
+						'No tienes los permisos requeridos para ejecutar este comando: '
+						+ ', '.join([f'`{perm}`' for perm in perms])
+					)
+
+				case commands.NSFWChannelRequired():
+					error_msg = 'Este comando solo se puede usar en canales NSFW'
+
+				case app_commands.CommandOnCooldown(retry_after=retry_after):
+					cooldown = core.fix_delta(timedelta(seconds=retry_after), ms=True)
+					error_msg = f'Tienes que esperar **{cooldown}** para ejecutar este comando de nuevo'
+
+				case app_commands.CommandInvokeError():
+					types = (
+						('(error code: 50013): Missing Permissions', 
+						   'No tengo los permisos requeridos para ejecutar este comando. '
+						   'Intenta añadirlos editando los permisos o la posición mi rol'),
+						('Unknown Message', 'El mensaje que se trató de editar/eliminar/reaccionar fue eliminado'),
+						('.value: Must be 1024 or fewer in length.', 'El valor de una sección de un embed no debe ser mayor a 1024 caracteres'),
+						('Must be 2000 or fewer in length', 'El mensaje no debe superar los 2000 caracteres'),
+						('Must be 4000 or fewer in length', 'El mensaje no debe superar los 4000 caracteres'),
+						('ValueError: Tag name limit reached', 'El límite de caracteres para el nombre un tag es de 32'),
+						('ValueError: Invalid characters detected', 'El nombre de un tag no puede contener espacios ni caracteres markdown'),
+						('ValueError: Invalid URL', 'URL inválida')
+					)
+					for error_note, message in types:
+						if error_note in note:
+							error_msg = message
+
+				case exceptions.DisabledTagsError():
+					error_msg = None
+				
+				case exceptions.ExistentTagError():
+					error_msg = 'Este tag ya existe'
+
+				case exceptions.ImageNotFound():
+					error_msg = 'No se encontró ninguna imagen en los últimos 30 mensajes'
+
+				case exceptions.NotOwner():
+					error_msg = 'Comando exclusivo del dueño del bot'
+
+				case exceptions.BlacklistUserError():
+					error_msg = 'Estás en la lista negra. No tienes permitido usar el bot'
+
+				case exceptions.NonExistentTagError():
+					if 'This tag' in note:
+						error_msg = 'Este tag no existe'
+					if 'This user' in note:
+						error_msg = 'Este usuario no tiene tags'
+					elif 'This server' in note:
+						error_msg = 'Este servidor no tiene tags'
+
+				# If the error remains unidentified, send a generic error message
+				case _:
+					embed = discord.Embed(
+						title=f'Ha ocurrido un error',
+						description=f'```py\n{repr(error)}\n```',
+						colour=db.default_color(interaction)
+					).set_author(
+						name=interaction.user.name,
+						icon_url=interaction.user.display_avatar.url
+					).set_footer(text='Este error ha sido notificado y será investigado para su posterior correción.')
+					if not interaction.response.is_done():
+						await interaction.response.send_message(embed=embed, ephemeral=True)
+					else:
+						await interaction.edit_original_response(content=None, embed=embed, attachments=[], view=None)
+						await (await interaction.original_response()).clear_reactions()
+					core.logger.error('An error has ocurred', exc_info=error)
+					return
 
 			# If the error message is an empty string, don't do anything
-			if error_msg == '':
+			if error_msg is None:
 				return
-			else:
-				# If the error message ends with '---', don't set a cooldown
-				try:
-					await interaction.response.send_message(core.Warning.error(error_msg), ephemeral=True)
-				except discord.InteractionResponded:
-					await interaction.followup.send(core.Warning.error(error_msg))
+
+			try:
+				await interaction.response.send_message(core.Warning.error(error_msg), ephemeral=True)
+			except discord.InteractionResponded:
+				await interaction.followup.send(core.Warning.error(error_msg))
 
 	@commands.Cog.listener('on_ready')
 	async def ready(self) -> None:
@@ -125,8 +143,8 @@ class Listeners(commands.Cog):
 		) -> None:
 		core.logger.info(
 			f'Se he usado un comando: "{command.name} {interaction.namespace}", '
-			f'servidor "{interaction.guild.name} <{interaction.guild.id}>", '
-			f'canal "{interaction.channel.name} <{interaction.channel.id}>" {interaction.data}'
+			f'servidor "{interaction.guild} <{interaction.guild.id if interaction.guild else "?"}>", '
+			f'canal "{interaction.channel} <{interaction.channel.id if interaction.channel else "?"}>" {interaction.data}'
 		)
 
 		if core.bot_mode == 'stable':
@@ -150,7 +168,7 @@ class Listeners(commands.Cog):
 
 
 	@commands.Cog.listener('on_guild_remove')
-	async def guild_join_logging(self, guild: discord.Guild) -> None:
+	async def guild_leave_logging(self, guild: discord.Guild) -> None:
 		core.logger.info(f'El bot ha salido de un servidor: {repr(guild)}')
 
 
